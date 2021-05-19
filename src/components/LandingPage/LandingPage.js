bf3ccbe64c09b290bdd51977ca331e87426f1944
srino <seenivasan9559@gmail.com>
2021-05-19 12:16:26 +0530

    'dchhcd'

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -51,7 +51,123 @@ class LandingPage extends Component {
               )}
             </div>
           </div>
-
+          <div className="mainWrapper">
+            <div className="col-md-5 col-sm-6 col-xs-12 noPadd">
+              <div className="appstackWrapper">
+                <div className="appStack">
+                  <div className="col-md-1 col-sm-1 col-xs-2 removePaddLeft flexWidth">
+                    <i className="em em---1" />
+                  </div>
+                  <div className="col-md-11 col-sm-11 col-xs-10 noPadd">
+                    <div className="description">
+                      Try out a realtime app that uses
+                    </div>
+                    <div className="appStackIconWrapper">
+                      <div className="col-md-4 col-sm-4 col-xs-4 noPadd">
+                        <div className="appStackIcon">
+                          <img
+                            className="img-responsive"
+                            src={reactLogo}
+                            alt="React logo"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-4 col-sm-4 col-xs-4 noPadd">
+                        <div className="appStackIcon">
+                          <img
+                            className="img-responsive"
+                            src={authLogo}
+                            alt="Auth0 logo"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-4 col-sm-4 col-xs-4 noPadd">
+                        <div className="appStackIcon">
+                          <img
+                            className="img-responsive"
+                            src={graphql}
+                            alt="GraphQL logo"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="appStack">
+                  <div className="col-md-1 col-sm-1 col-xs-2 removePaddLeft flexWidth">
+                    <i className="em em-rocket" />
+                  </div>
+                  <div className="col-md-11 col-sm-11 col-xs-10 noPadd">
+                    <div className="description">Powered by</div>
+                    <div className="appStackIconWrapper">
+                      <div className="col-md-4 col-sm-4 col-xs-4 noPadd">
+                        <div className="appStackIcon">
+                          <img
+                            className="img-responsive"
+                            src={apolloLogo}
+                            alt="apollo logo"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-4 col-sm-4 col-xs-4 noPadd">
+                        <div className="appStackIcon">
+                          <img
+                            className="img-responsive"
+                            src={hasuraLogo}
+                            alt="Hasura logo"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="appStack">
+                  <div className="col-md-1 col-sm-1 col-xs-2 removePaddLeft flexWidth">
+                    <i className="em em-sunglasses" />
+                  </div>
+                  <div className="col-md-11 col-sm-11 col-xs-10 noPadd">
+                    <div className="description removePaddBottom">
+                      Explore the Hasura GraphQL backend and try out some
+                      queries & mutations
+                    </div>
+                  </div>
+                </div>
+                <div className="appStack removePaddBottom">
+                  <div className="col-md-1 col-sm-1 col-xs-2 removePaddLeft flexWidth">
+                    <i className="em em-zap" />
+                  </div>
+                  <div className="col-md-11 col-sm-11 col-xs-10 noPadd">
+                    <div className="description removePaddBottom">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://auth0.com/blog/building-a-collaborative-todo-app-with-realtime-graphql-using-hasura/"
+                      >
+                        Full tutorial with Auth0!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/*
+              <div className="footer">
+                Built with
+                <i className="fas fa-heart" />
+                by{" "}
+                <a
+                  href="https://hasura.io/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Hasura
+                </a>
+              </div>
+              */}
+            </div>
+            <div className="tutorialImg col-md-6 col-sm-6 col-xs-12 hidden-xs noPadd">
+              <img className="img-responsive" src={rightImg} alt="View" />
+            </div>
+          </div>
         </div>
       </div>
     );
